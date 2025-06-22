88fde1cd7146d4c4552fe6d21bc2dee7d41c57cb
Rudrank Riyam <rudrankriyam@gmail.com>
2025-06-21 23:50:31 -0700

    Add macOS specific window size settings

--- FMF/FMF/FMFApp.swift
+++ FMF/FMF/FMFApp.swift
@@ -12,6 +12,12 @@ struct FMFApp: App {
   var body: some Scene {
     WindowGroup {
       ContentView()
+#if os(macOS)
+        .frame(minWidth: 800, minHeight: 600)
+#endif
     }
+#if os(macOS)
+    .defaultSize(width: 1000, height: 700)
+#endif
   }
 }
