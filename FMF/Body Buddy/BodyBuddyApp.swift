b2d159cfdda71f63fa3f3a2e3692dba2d1cdf0fc
Rudrank Riyam <rudrankriyam@gmail.com>
2025-06-23 17:39:03 +0200

    Add initial Body Buddy SwiftUI app

---
+++ FMF/Body Buddy/BodyBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyBuddyApp.swift
+//  Body Buddy
+//
+//  Created by Rudrank Riyam on 6/23/25.
+//
+
+import SwiftUI
+
+@main
+struct BodyBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
