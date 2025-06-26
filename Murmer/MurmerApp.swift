22ac91cd05dcb443078b4a2e102dbd50b4ed4209
Rudrank Riyam <rudrankriyam@gmail.com>
2025-06-26 19:11:28 +0530

    Add initial SwiftUI app for Murmer

---
+++ Murmer/MurmerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MurmerApp.swift
+//  Murmer
+//
+//  Created by Rudrank Riyam on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct MurmerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
