c9e7e66a4bf21a9d6143c0caf322fdfe871ff8db
Rudrank Riyam <rudrankriyam@gmail.com>
2025-10-27 15:42:08 +0530

    Remove Adapter Studio project files and related assets

--- Adapter Studio/adapter_cli/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Adapter Studio CLI - Python package for adapter training workflows"""
-
-__version__ = "0.1.0"
-__author__ = "Rudrank Riyam"
-
-from .banner import print_banner, BANNER
-
-__all__ = ["print_banner", "BANNER"]
